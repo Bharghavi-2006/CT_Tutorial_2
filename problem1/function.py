a50cd64bd343921b041f01ec526f993e9d4a8e27
Bharghavi-2006 <dvbiitmadras@gmail.com>
2025-08-28 17:07:16 +0530

    final change

--- problem1/function.py
+++ problem1/function.py
@@ -1,12 +1,6 @@
 def f(x):
-<<<<<<< HEAD
-    return 729*(x**3) - 432*(x) + 128
-def f_d(x):
-    return 2187*(x**2) - 432
-=======
     return 729*x**3 - 432*x + 128
 def f_d(x):
     return 2187*x**2 - 432
 def f_dd(x):
     return 4274*x
->>>>>>> d234a0051b47b1c8d548230a9bfd78992bde429e
